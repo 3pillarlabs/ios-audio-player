f2046ecf3291eb0f00187d2e0795a63d423d2b99
Tudor Dan Stanean <tudor.stanean@3pillarglobal.com>
2016-09-26 16:37:11 +0300

    Removed unused files

--- Framework/iOSAudioPlayer/DerivedData/iOSAudioPlayer/Build/Intermediates/iOSAudioPlayer.build/Debug-iphonesimulator/iOSAudioPlayer.build/Objects-normal/i386/iOSAudioPlayer_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/tudor.stanean/Development/LabsDevelopment/iOS-AudioPlayer-Github/Framework/iOSAudioPlayer/DerivedData/iOSAudioPlayer/Build/Intermediates/iOSAudioPlayer.build/Debug-iphonesimulator/iOSAudioPlayer.build/DerivedSources/iOSAudioPlayer_vers.c
