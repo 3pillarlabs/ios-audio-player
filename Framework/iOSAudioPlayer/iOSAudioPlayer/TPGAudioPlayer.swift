7876fa528c18ad54d734ce781941bb55f162ac36
David Livadaru <david.livadaru@3pillarglobal.com>
2018-04-17 14:03:48 +0300

    Migrate-To-Swift4
    Move notifications on a separate
    Use block based KVO.
    Update project to Xcode 9.3.

--- Framework/iOSAudioPlayer/iOSAudioPlayer/TPGAudioPlayer.swift
+++ Framework/iOSAudioPlayer/iOSAudioPlayer/TPGAudioPlayer.swift
@@ -9,42 +9,9 @@
 import UIKit
 import AVFoundation
 
-/*
-    TPGMediaLoadedStateNotification - triggered each time an media hunk was loaded in queue
-*/
-
-public let TPGMediaLoadedStateNotification = "com.3pillarglobal.medialoadedstate.notification"
-
-/*
-    TPGPlayerStalledNotification - indicates that the player has stalled
-        -> object: NSNumber indicating the current player item
-*/
-
-public let TPGPlayerStalledNotification = "com.3pillarglobal.playerstalled.notification"
-
-/*
-    TPGPlayerDidReachEndNotification - player has reached end
-*/
-
-public let TPGPlayerDidReachEndNotification = "com.3pillarglobal.playerdidreachend.notification"
-
-/*
-    TPGPlayerTimeJumpedNotification - player has jumped to a different
-        -> object: NSNumber indicating the current time the player has jumped to
-*/
-
-public let TPGPlayerTimeJumpedNotification = "com.3pillarglobal.playertimejumped.notification"
-
-let kStartTimeZero                      = 0.0
-
-public let kLoadedTimeRangesKeyPath    = "loadedTimeRanges"
-
-/*
-    SkipDirection type used in skipDirection: method
-*/
-
+/// Used in skipDirection: method.
 public enum SkipDirection: Double {
-    case Backward = -1, Forward = 1
+    case backward = -1, forward = 1
 }
 
 public class TPGAudioPlayer: NSObject {
@@ -75,34 +42,23 @@ public class TPGAudioPlayer: NSObject {
             }
         }
     }
+
+    private var timeRangesObservation: Any?
+
+    // MARK: PUBLIC METHODS
     
-    /*************************************/
-            // MARK: PUBLIC METHODS
-    /*************************************/
-     
-    
-    /*
-        Method used to return the total duration of the player item
-    */
-    
+    /// Method used to return the total duration of the player item.
     public var durationInSeconds: Double {
         get {
             return playerDuration ?? kCMTimeZero.seconds
         }
     }
 
-    /*
-        Method used to return the total duration of the player item
-    */
-    
     public class func sharedInstance() -> TPGAudioPlayer {
         return self.internalInstance
     }
-    
-    /*
-        Returns the current time in seconds of the current player item
-    */
         
+    /// Current time in seconds of the current player item.
     public var currentTimeInSeconds: Double {
         get {
             return player.currentTime().seconds
@@ -115,23 +71,23 @@ public class TPGAudioPlayer: NSObject {
         self.setupNotifications()
     }
     
-    /*
-        Method to be called whenever play or pause functionality is needed:
-            -> audioUrl: the resource that need to be processed
-            -> springboardInfo: dictionary that contains useful information to be displayed when device is in sleep mode
-                + kTitleKey - holds value of title String object
-                + kAuthorKey - key for holding the author information
-                + kDurationKey - length of the certain player item
-                + kListScreenTitleKey - secondary information to be displayed on springboard in sleep mode
-                + kImagePathKey - key to be used for holding the path of image to be displayed
-    
-            -> startTime: offset from where the certain processing should start
-            -> completion: completion block
-    
-        NOTE: springboardInfo is an optional parameter, it can be set to nil if the feature of playing in background while 
-        the device is in sleep mode is desired to be ignored.
-    */
-    
+    /// Method to be called whenever play or pause functionality is needed.
+    ///
+    /// __springboardInfo__ may contain the following keys:
+    ///     - kTitleKey: holds value of title String object
+    ///     - kAuthorKey - key for holding the author information
+    ///     - kDurationKey - length of the certain player item
+    ///     - kListScreenTitleKey - secondary information to be displayed on springboard in sleep mode
+    ///     - kImagePathKey - key to be used for holding the path of image to be displayed
+    ///
+    /// *springboardInfo* is an optional parameter, it can be set to nil if the feature of playing in background while
+    /// the device is in sleep mode is desired to be ignored.
+    ///
+    /// - Parameters:
+    ///   - audioUrl: The resource that need to be processed.
+    ///   - springboardInfo: dictionary that contains useful information to be displayed when device is in sleep mode
+    ///   - startTime: Offset from where the certain processing should start
+    ///   - completion: Completion block.
     public func playPauseMediaFile(audioUrl: NSURL, springboardInfo: Dictionary <String, AnyObject>, startTime: Double, completion: @escaping (_ previousItem: String?, _ stopTime: Double) -> ()) {
         
         let stopTime = self.currentTimeInSeconds
@@ -194,41 +150,23 @@ public class TPGAudioPlayer: NSObject {
             }
         })
     }
-   
-    /*
-        Method used for showing the buffer bar (i.e. amount of the playable file that's been loaded)
-    */
-    
-   override public func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-        if keyPath == kLoadedTimeRangesKeyPath && object as? AVPlayerItem === self.player.currentItem {
-            if let timeRanges = change?[NSKeyValueChangeKey.newKey] as? Array<AnyObject> {
-                if timeRanges.count > 0 {
-                    let timeRange = timeRanges[0].timeRangeValue
-                    let loadedAmout = (timeRange?.start.seconds)! + (timeRange?.duration.seconds)!
-                    let loadedPercentage = (loadedAmout * 100.0) / self.durationInSeconds
-                    
-                    NotificationCenter.default.post(name: NSNotification.Name(rawValue: TPGMediaLoadedStateNotification), object: NSNumber(value: loadedPercentage))
-                }
-            }
-        }
-    }
     
     /*************************************/
         // MARK: NOTIFICATION METHODS
     /*************************************/
     
     @objc func playingStalled(_ notification: NSNotification) {
-        NotificationCenter.default.post(name: NSNotification.Name(rawValue: TPGPlayerStalledNotification), object: player.currentItem)
+        NotificationCenter.default.post(name: .playerStalled, object: player.currentItem)
     }
     
     @objc func playerDidReachEnd(_ notification: NSNotification) {
         self.player.seek(to: kCMTimeZero)
         
-        NotificationCenter.default.post(name: NSNotification.Name(rawValue: TPGPlayerDidReachEndNotification), object: nil)
+        NotificationCenter.default.post(name: .playerDidReachEnd, object: nil)
     }
     
     @objc func playerItemTimeJumpedNotification(_ notification: NSNotification) {
-        NotificationCenter.default.post(name: NSNotification.Name(rawValue: TPGPlayerTimeJumpedNotification), object: NSNumber(value: self.currentTimeInSeconds))
+        NotificationCenter.default.post(name: .playerTimeDidChange, object: NSNumber(value: self.currentTimeInSeconds))
     }
     
     /*************************************/
@@ -254,25 +192,33 @@ public class TPGAudioPlayer: NSObject {
         
         self.player.pause()
         
-        //remove kvo for current item
-        let currentItem = self.player.currentItem
-        currentItem?.removeObserver(self, forKeyPath: kLoadedTimeRangesKeyPath, context: nil)
+        // remove kvo for current item
+        timeRangesObservation = nil
         
-        //load new asset
+        // load new asset
         let newPlayerItem = AVPlayerItem(asset: playerAsset)
         
-        //replace current item with new player item
+        // replace current item with new player item
         self.player.replaceCurrentItem(with: newPlayerItem)
         
         if let _ = self.player.currentItem {
-            self.player.currentItem!.addObserver(self, forKeyPath: kLoadedTimeRangesKeyPath, options: .new, context: nil)
+            timeRangesObservation = player.currentItem?.observe(\.loadedTimeRanges, changeHandler: { (playerItem, change) in
+                guard let timeRanges = change.newValue else { return }
+                guard let firstTimeRange = timeRanges.first else { return }
+
+                let timeRange = firstTimeRange.timeRangeValue
+                let loadedAmout = timeRange.start.seconds + timeRange.duration.seconds
+                let loadedPercentage = (loadedAmout * 100.0) / self.durationInSeconds
+
+                NotificationCenter.default.post(name: .mediaLoadProgress, object: NSNumber(value: loadedPercentage))
+            })
         }
         
-        //seek player to offset
+        // seek player to offset
         self.seekPlayerToTime(value: startTime, completion: { [unowned self] in
             self.player.play()
             
             completion()
-            })
+        })
     }
 }
